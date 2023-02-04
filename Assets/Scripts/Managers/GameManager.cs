94f5617af7ae8d95bc540c704954c0df22d04b84
mockba1501 <7790976+mockba1501@users.noreply.github.com>
2023-02-04 22:58:45 +0200

    Word & ItemSlot Scripts

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,23 +6,48 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     //Initialize the game with a list all possible scriptable object
-    //[SerializeField]
     public List<RootWord> words;
-
+    public Queue<Tuple<string, wordTypes>> wordsQueue;
+    public enum wordTypes
+    {
+        Root,
+        Prefix,
+        Suffix
+    }
     // Start is called before the first frame update
     void Start()
     {
         //When you start the game you need to populate information from the scriptable data to formulate the words
         //Create a queue structure to include the words 
+        PopulateWordOptions();
 
     //    string root = words[0].rootWord.ToString();
-     //   Debug.Log(root);
-        
+    //   Debug.Log(root);
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    public void PopulateWordOptions()
+    {
+        int countRoots = words.Count;
+        //int countGeneratedWords = 0;
+
+        Debug.Log(countRoots);
+        
+        //string root = words[0].rootWord.ToString();
+        //Generate around 20 words
+        
+        for (int i =0; i<countRoots; i++) 
+        {
+            wordsQueue.Enqueue(new Tuple<string, wordTypes>( words[i].rootWord.ToString(), 0));
+            
+            
+        }
         
     }
 }
--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public static UIManager uiManager;
+    public Transform itemsOnSale;
+
+    ItemSlot[] slots;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        slots = itemsOnSale.GetComponentsInChildren<ItemSlot>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //
+    public void InitilizeWordSlots()
+    {
+
     }
 }
