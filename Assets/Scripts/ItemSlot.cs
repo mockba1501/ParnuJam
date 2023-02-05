2acc80e82772531b9f66841a2ce3141f6090046c
Yoshio-0 <90753067+Yoshio-0@users.noreply.github.com>
2023-02-05 12:54:13 +0200

    Updated ItemSlot.cs

--- Assets/Scripts/ItemSlot.cs
+++ Assets/Scripts/ItemSlot.cs
@@ -7,18 +7,27 @@ using static UnityEditor.Progress;
 public class ItemSlot : MonoBehaviour
 {
 
-    public Image icon;
+    public Sprite icon; // Previsously Image class
     public Button removeButton;
-    public Text wordLabel;
+    public string wordLabel; // Previously Text class
     public int type; //0 root, 1 prefix, 2 suffix
     public WordItem wordItem;
+
+    public UIManager uiMngr;
+
     public void AddItem(WordItem newItem)
     {
         this.wordItem = newItem;
-        
-        if(newItem.type == 0)
-        {
 
+        wordLabel = wordItem.word;
+
+        if (newItem.type == 0)
+        {
+            icon = uiMngr.seedBagImg;
+        }
+        else if (newItem.type == 1 || newItem.type == 2)
+        {
+            icon = uiMngr.fertilizerBagImg;
         }
     }
 }
