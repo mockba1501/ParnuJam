bae68718c028bcf631a4e37f992c4ba7229ace6a
mockba1501 <7790976+mockba1501@users.noreply.github.com>
2023-02-05 02:02:39 +0200

    Word Generation Logic
    Roots, Prefix, Suffix words automatically generated from the scriptable objects and stored in a queue data structure

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Unity.Burst.Intrinsics.X86.Avx;
+using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
     //Initialize the game with a list all possible scriptable object
     public List<RootWord> words;
-    public Queue<Tuple<string, wordTypes>> wordsQueue;
+    public Queue<Tuple<string, wordTypes>> wordsQueue = new Queue<Tuple<string, wordTypes>>();
     public enum wordTypes
     {
         Root,
@@ -20,10 +22,8 @@ public class GameManager : MonoBehaviour
         //When you start the game you need to populate information from the scriptable data to formulate the words
         //Create a queue structure to include the words 
         PopulateWordOptions();
-
-    //    string root = words[0].rootWord.ToString();
-    //   Debug.Log(root);
-
+        //PrintWords();
+    
     }
 
     // Update is called once per frame
@@ -34,20 +34,79 @@ public class GameManager : MonoBehaviour
 
     public void PopulateWordOptions()
     {
-        int countRoots = words.Count;
-        //int countGeneratedWords = 0;
+        int totalRoots = words.Count;
+        int countRoots = 0;
+        int countGeneratedWords = 0;
 
-        Debug.Log(countRoots);
-        
-        //string root = words[0].rootWord.ToString();
-        //Generate around 20 words
-        
-        for (int i =0; i<countRoots; i++) 
+        //Stop after generating 20 roots or after passing over all roots
+        while (countGeneratedWords < 20  && countRoots <totalRoots)
         {
-            wordsQueue.Enqueue(new Tuple<string, wordTypes>( words[i].rootWord.ToString(), 0));
+            //Add the root word
+            string tmp = words[countRoots].rootWord.ToString();
+            wordsQueue.Enqueue(new Tuple<string, wordTypes>(tmp, wordTypes.Root));
+            countGeneratedWords++;
+            Debug.Log($"Word {countGeneratedWords} Adding root {tmp}");
+            //Extract the prefix words
+            List<string> tmpPrefix = new List<string>(words[countRoots].prefixList);
+            int tmpPrefixCount = tmpPrefix.Count;
+            int prefixCount = 0;
             
-            
-        }
+            //Make sure that the list actually contains some words
+            if (tmpPrefixCount > 0)
+            {
+                //Loop until you reach half the items or a max of two items
+                while (/*prefixCount <= tmpPrefixCount / 2 && */ prefixCount < 1)
+                {
+                    int randomIndex = Random.Range(0, tmpPrefix.Count);
+                    tmp = tmpPrefix[randomIndex];
+
+                    wordsQueue.Enqueue(new Tuple<string, wordTypes>(tmp, wordTypes.Prefix));
+                    tmpPrefix.RemoveAt(randomIndex);
+                    
+                    countGeneratedWords++;
+                    prefixCount++;
+                    Debug.Log($"Word {countGeneratedWords} Adding prefix {tmp}");
+                }
+            }
+
+            //Extract and add the suffix words
+            List<string> tmpSuffix = new List<string>(words[countRoots].suffixList);
+            int tmpSuffixCount = tmpSuffix.Count;
+            int suffixCount = 0;
+
+
+            //Make sure that the list actually contains some words
+            if (tmpSuffixCount > 0)
+            {
+                //Loop until you reach half the items or a max of two items
+                while (suffixCount <= tmpSuffixCount / 2 && suffixCount < 3)
+                {
+                    int randomIndex = Random.Range(0, tmpSuffix.Count);
+                    tmp = tmpSuffix[randomIndex];
+
+                    wordsQueue.Enqueue(new Tuple<string, wordTypes>(tmp, wordTypes.Suffix));
+                    tmpSuffix.RemoveAt(randomIndex);
+                    
+                    countGeneratedWords++;
+                    suffixCount++;
+                    Debug.Log($"Word {countGeneratedWords} Adding suffix {tmp}");
+
+                }
+            }
+            countRoots++;
+        }//end of word generation
         
     }
+
+    public void PrintWords()
+    {
+        int i = 0;
+        //foreach(string in wordsQueue)
+        foreach(var data in wordsQueue)
+        { 
+            Debug.Log($"word {i} {data.Item1} of type {data.Item2}");
+            i++;
+        }
+    }
+
 }
