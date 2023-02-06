1656f2dcc478771aba3f2384d4bd09005dfdfd97
mockba1501 <7790976+mockba1501@users.noreply.github.com>
2023-02-06 21:21:49 +0200

    Cleaned ItemSlot script
    Removed local images from within the item slot
    Instead using icon images (seed and fertilizer) bags from the UI manager

--- Assets/Scripts/ItemSlot.cs
+++ Assets/Scripts/ItemSlot.cs
@@ -6,65 +6,46 @@ using UnityEngine.UI;
 using TMPro;
 
 public class ItemSlot : MonoBehaviour
-{
-    public Sprite seedBagImg; // previously Image class
-    public Sprite fertilizerBagImg; // previously Image class
-    
+{   
     public Image icon; // Previsously Image class
     //public Button removeButton;
     public Button useButton;
     public TMP_Text buttonText;
 
-    //public string wordLabel; // Previously Text class
-    public int type; //0 root, 1 prefix, 2 suffix
+    //word item type values: //0 root, 1 prefix, 2 suffix
     public WordItem wordItem;
 
     public UIManager uiMngr;
     public PlantManager plantManager;
 
-    
-
+    /*
     private void Start()
     {
-      //  icon = useButton.GetComponentInChildren<Image>();
-
-        // gObjectToFind = transform.GetChild(0).GetChild(0).gameObject;
-        //Debug.Log(gObjectToFind.GetComponent<TMP_Text>().text);
-
-        //newSprite = objectToFind.GetComponent<Image>().sprite;
-        //Debug.Log(newSprite.name);
-
-        //icon = objectToFind.GetComponent<Image>();
-
-        //buttonText = gObjectToFind.GetComponent<TMP_Text>().text;
-
     }
+    */
 
     //Adding a new word to the item slot
     public void AddItem(WordItem newItem)
     {
         this.wordItem = newItem;
-        this.type = newItem.type;
-
        
         buttonText.text = wordItem.word;
-        Debug.Log(newItem.type.GetType());
 
-        if (this.type == 0)
+        if (newItem.type == 0)
         {
-            // icon.sprite = uiMngr.seedBagImg;
-            icon.sprite = seedBagImg;
+             icon.sprite = uiMngr.seedBagImg;
         }
         else
         {
-            //icon.sprite = uiMngr.fertilizerBagImg;
-            icon.sprite = fertilizerBagImg;
+            icon.sprite = uiMngr.fertilizerBagImg;
         }
     }
 
     public void UseItem()
     {
         plantManager.EnablePlant(0);
+        ClearSlot();
+        //AddNextWord();
         /*
         //When you click on an item either two things happen:
         //1) If it was a root word and there are empty spaces in the field plant it
@@ -73,17 +54,13 @@ public class ItemSlot : MonoBehaviour
             a) you select a correct root combination it will grow to the following level
             b) if incorrect root nothing will happen
 
-
+        //3) Clear the slot and add a new word in its place
         */
     }
 
-    /*
     public void ClearSlot()
     {
-        //this.wordItem = null;
-        icon.sprite = null;
-        icon.enabled = false;
-        removeButton.interactable = false;
+        this.gameObject.SetActive(false);
+        
     }
-    */
 }
